da883181fe2fb07997a24898903bf875aeecd9b4
ScarlettJK <abigail_puentes_23@comunidad.unam.mx>
2026-04-07 08:53:33 -0600

    Actualizacion de pantallas onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    //BULDING FEATURES 07/04/2026
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -41,6 +47,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
